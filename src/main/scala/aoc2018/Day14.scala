764cc294c6176419f7f537bf8db8c24e63de2b69
Michel Edkrantz <me@webic.se>
2018-12-14 23:14:50 +0100

    prettier

--- src/main/scala/aoc2018/Day14.scala
+++ src/main/scala/aoc2018/Day14.scala
@@ -9,16 +9,15 @@ object Day14 extends App {
   def run() = {
     val recipies = ArrayBuffer[Int](3, 7)
     var elves = Vector(0, 1)
-    val it = Iterator.from(0).flatMap { case(t) =>
-      if(t % 1000000 == 0)
-        println(s"t=$t")
+    recipies.iterator ++ Iterator.from(0).flatMap { t =>
+//      if(t % 1000000 == 0)
+//        println(s"t=$t")
       //println(s"${recipies.mkString(" ")}")
       val ints = elves.map(recipies).sum.toString.map(_.toInt - 48)
       recipies.appendAll(ints)
       elves = elves.map(e => (e + 1 + recipies(e)) % recipies.length)
       ints
     }
-    recipies.iterator ++ it
   }
 
   def part1(n: Int): String = run().slice(n, n + 10).mkString
@@ -28,14 +27,14 @@ object Day14 extends App {
     run().sliding(seq.length).zipWithIndex.find(_._1 == s).head._2
   }
 
-  println(part1(9), "5158916779")
-//  println(part1(5), "0124515891")
-//  println(part1(18), "9251071085")
-//  println(part1(6), "9251071085")
- println(part1(input.toInt))
- println(part2("59414"), 2018)
- // println(part2("92510"), 18)
+  // println(part1(9), "5158916779")
+  //  println(part1(5), "0124515891")
+  //  println(part1(18), "9251071085")
+  //  println(part1(6), "9251071085")
+  //  println(part2("59414"), 2018)
+  // println(part2("92510"), 18)
+
+  println(part1(input.toInt))
   println(part2(input))
-  //println(run("37", 2018), "5941429882")
 
 }
