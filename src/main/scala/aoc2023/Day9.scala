49a0fecfad1fcf46f942a688054a554553800493
Michel Edkrantz <michel.edkrantz@kognic.com>
2023-12-17 15:12:05 +0100

    solve day 9

---
+++ src/main/scala/aoc2023/Day9.scala
@@ -0,0 +1,46 @@
+package aoc2023
+
+import tools.AocDay
+
+object Day9 extends App with AocDay {
+  val (year, day) = (2023, 9)
+
+  def parse(instructions: Seq[String]): Seq[Vector[Int]] =
+    instructions.map(_.split(" ").map(_.toInt).toVector).toVector
+
+  def layerDiff(in: List[Vector[Int]]): List[Vector[Int]] = {
+    val diff = in.last.sliding(2).map(x => x(1) - x(0)).toVector
+    val allZero = diff.forall(_ == 0)
+    if (allZero) {
+      in
+    } else {
+      layerDiff(in ::: List(diff))
+    }
+  }
+
+  def solveOne(line: Vector[Int]): Int = {
+    layerDiff(List(line)).map(_.last).sum
+  }
+
+  def solveOneB(line: Vector[Int]): Int = {
+    layerDiff(List(line)).map(_.head).reduceRight((a, b) => a - b)
+  }
+
+  def solveProblemA(input: Seq[Vector[Int]]): Int = {
+    input.map(solveOne).sum
+  }
+
+  def solveProblemB(input: Seq[Vector[Int]]): Int = {
+    input.map(solveOneB).sum
+  }
+
+  val testInput = parse(testInstructions)
+  println(solveProblemA(testInput))
+  println(solveProblemB(testInput))
+
+  val input = parse(instructions)
+  println(solveProblemA(input))
+  println(solveProblemB(input))
+
+
+}
