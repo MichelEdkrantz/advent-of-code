d184549e17b989919bf1bbb6b41da1fb04245910
Michel Edkrantz <michel.edkrantz@kognic.com>
2023-12-19 07:09:13 +0100

    rewrite with simple path finder

--- src/main/scala/aoc2023/Day10.scala
+++ src/main/scala/aoc2023/Day10.scala
@@ -2,18 +2,11 @@ package aoc2023
 
 import tools.AocDay
 
-import scala.collection.mutable
-
 object Day10 extends App with AocDay {
   val (year, day) = (2023, 10)
   type CharGrid = Vector[Vector[Char]]
 
   def parse(instructions: List[String]): CharGrid = instructions.map(_.toVector).toVector
-
-  val north = '|' :: '7' :: 'F' :: Nil
-  val south = '|' :: 'J' :: 'L' :: Nil
-  val east = '-' :: '7' :: 'J' :: Nil
-  val west = '-' :: 'L' :: 'F' :: Nil
 
   case class Position(x: Int, y: Int)
 
@@ -23,32 +16,53 @@ object Day10 extends App with AocDay {
     if cell == 'S'
   } yield Position(x, y)).head
 
-  def createDistanceMap(grid: CharGrid): mutable.Map[Position, Int] = {
-    mutable.Map() ++ (for {
-      (row, y) <- grid.zipWithIndex
-      (_, x) <- row.zipWithIndex
-    } yield (Position(x, y) -> Int.MaxValue)).toMap
+  // can this be done with complex numbers or some kind of rotation?
+  def next(current: Char, direction: Char): (Position, Char) = (current, direction) match {
+    case ('|', 'N') => (Position(0, -1), 'N')
+    case ('|', 'S') => (Position(0, 1), 'S')
+    case ('-', 'E') => (Position(1, 0), 'E')
+    case ('-', 'W') => (Position(-1, 0), 'W')
+    case ('F', 'N') => (Position(1, 0), 'E')
+    case ('F', 'W') => (Position(0, 1), 'S')
+    case ('7', 'E') => (Position(0, 1), 'S')
+    case ('7', 'N') => (Position(-1, 0), 'W')
+    case ('J', 'S') => (Position(-1, 0), 'W')
+    case ('J', 'E') => (Position(0, -1), 'N')
+    case ('L', 'W') => (Position(0, -1), 'N')
+    case ('L', 'S') => (Position(1, 0), 'E')
   }
 
-  def checkBounds(grid: CharGrid, pos: Position): Boolean = {
+  /*def checkBounds(grid: CharGrid, pos: Position): Boolean = {
     val (x, y) = (pos.x, pos.y)
     x >= 0 && x < grid.head.length && y >= 0 && y < grid.length
+  }*/
+
+  def findPath(grid: CharGrid, start: Position): List[Position] = {
+    def walk(direction: Char, path: List[Position]): List[Position] = {
+      val current = path.head
+      val currentChar = grid(current.y)(current.x)
+      if (currentChar == 'S') {
+        path.tail
+      } else {
+        val (delta, dir) = next(currentChar, direction)
+        val nextPos = Position(current.x + delta.x, current.y + delta.y)
+        walk(dir, nextPos :: path)
+      }
+    }
+    // this assumes we always walk east
+    walk('E', start.copy(x = start.x + 1) :: start :: Nil)
   }
 
-  // TODO, this code is flawed. Using djiikstra's algorithm for part 1 is overkill, since we only need to follow the path
-  // it never forks, so we can just follow the path and keep track of the distance
-  // for part b, getting the path will be for free.
-
   def solveProblemA(grid: CharGrid): Int = {
-
+    val start = findStart(grid)
+    val path = findPath(grid, start)
+    path.length / 2
   }
 
   val testInput = parse(testInstructions)
   val test2Input = parse(test2Instructions)
-  val test3Input = parse(test3Instructions)
   val input = parse(instructions)
   println(solveProblemA(testInput))
-  println(solveProblemA(test2Input))
-  println(solveProblemA(test3Input))
+  //println(solveProblemA(test2Input))
   println(solveProblemA(input))
 }
